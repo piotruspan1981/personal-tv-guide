bc14ba601a9645ad84c07cf7033b6b8d499b12fd
liekelevdheide@gmail.com <liekelevdheide@gmail.com@20316321-1138-d48c-656c-dc0dae90915a>
2014-01-12 23:49:51 +0000

    Werkend Register met Email veld. Begin gemaakt met Shoutbox.

---
+++ PersonalTVGuide/PersonalTVGuide/Models/ShoutboxModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PersonalTVGuide.Models
+{
+    public class ShoutboxModels
+    {
+        public int ID { get; set; }
+        public int UID { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class ShoutboxDBContext : DbContext
+    {
+        public DbSet<ShoutboxModels> Shoutbox { get; set; }
+    }
+}
