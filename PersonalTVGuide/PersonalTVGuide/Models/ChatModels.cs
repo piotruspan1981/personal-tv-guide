600ef96053b61cd53ceec622d638f7fd0234bb62
franktvs91 <franktvs91@gmail.com>
2014-03-03 15:44:30 +0000

    Text toevoegen aan shoutbox database, database gemaakt via migrations.

--- PersonalTVGuide/PersonalTVGuide/Models/ChatModels.cs
+++ PersonalTVGuide/PersonalTVGuide/Models/ChatModels.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -16,20 +18,29 @@ namespace PersonalTVGuide.Models
         public DbSet<Shoutbox> Shoutbox{ get; set; }
         public DbSet<PrivateMsg> PrivateMsg { get; set; }
     }
+
+    [Table("Shoutbox")]
     public class Shoutbox
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public int UID { get; set; }
         public DateTime DateAndTime { get; set; }
+        [Required]
         public string Text { get; set; }
     }
 
+    [Table("PrivateMsg")]
     public class PrivateMsg
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int MsgID { get; set; }
         public int SenderID { get; set; }
         public int ReceiverID {get; set;}
         public DateTime DateAndTime { get; set; }
+        [Required]
         public string Text { get; set; }
         public bool Opened{ get; set; }
     }
